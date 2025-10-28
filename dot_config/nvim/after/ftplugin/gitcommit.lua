8cf4c5034410c7faea8a515ae6aab32dda80147e
Mario Hegyi <29924068+m-hegyi@users.noreply.github.com>
2025-10-28 15:38:45 +0100

    Add gitcommit filetype config for colorcolumn at 75 chars

---
+++ dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,4 @@
+-- Git commit message settings
+vim.opt_local.colorcolumn = "75"  -- Show vertical line at 75 characters
+vim.opt_local.textwidth = 72      -- Auto-wrap at 72 characters
+vim.opt_local.spell = true         -- Enable spell checking for commits
