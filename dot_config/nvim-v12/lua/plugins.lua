cf3efa375870f7a1ec4eb2f8b9fbe27e9593c3cc
Mario Hegyi <29924068+m-hegyi@users.noreply.github.com>
2026-09-10 16:46:49 +0200

    add copilot

--- dot_config/nvim-v12/lua/plugins.lua
+++ dot_config/nvim-v12/lua/plugins.lua
@@ -16,6 +16,7 @@ vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.surround' },
   { src = 'https://github.com/windwp/nvim-ts-autotag' },
   { src = 'https://github.com/catppuccin/nvim' },
+  { src = 'https://github.com/github/copilot.vim' },
 })
 
 require('mason').setup()
